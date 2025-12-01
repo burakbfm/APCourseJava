ef739f35f8745a1f0f1a838c2ec3dd9778ba966b
Burak Üstün <throborexz@gmail.com>
2025-12-01 22:54:41 +0300

    s
    s

--- Ders29_30/odev2.java
+++ Ders29_30/odev2.java
@@ -6,7 +6,16 @@ public class odev2 {
         // toplama yerine çarpma kullanılan bir biriktirici (aggregator) kalıbıdır.Toplam değişkenini 0’dan
         // başlattığımız gibi, çarpım değişkenini etkisiz eleman olan 1’den başlatmaları gerektiğini ipucu
         // olarakverebilirsiniz.
-        
+
+        int toplam = 0;
+        int sayac = 1;
+
+        while (sayac <= 5) {
+            toplam *= sayac; // toplam = toplam + sayac;
+            sayac++; // sayac = sayac + 1;
+        }
+        System.out.println("1'den 5'e kadar olan sayıların Çarpımı: " + toplam);
+
 
     }
 }
