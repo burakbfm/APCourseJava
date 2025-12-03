45c7d38bb3d6b2b8575b42f2653d23c0b91f0816
burakustunbfm <burakbfm@gmail.com>
2025-12-03 20:09:51 +0300

    s
    s

---
+++ Ders31_32/ForDongusuSayac.java
@@ -0,0 +1,27 @@
+package Ders31_32;
+
+public class ForDongusuSayac {
+    public static void main(String[] var0) {
+
+        // for (başlatma; koşul; güncelleme) {
+        //      Tekrar edecek kodlar
+        //  }
+
+        // 1. Başlatma:     int i = 1
+        // 2. Koşul:        i <=10
+        // 3. Güncelleme:   i++
+        System.out.println("--- FOR DÖNGÜSÜ İLE 1'DEN 10'A KADAR SAYMA ---");
+
+        for (int i = 1; i <= 10;i++ ) {
+            System.out.println("Sayı: " + i);
+        }
+
+        // int i = 1; // 1. BAŞLATMA
+        // while (i <=10) { // 2. KOŞUL
+        //     System.out.println("Sayı: " + i);
+        //     i ++; // 3. GÜNCELLEME
+        // }
+
+
+    }
+}
---
+++ Ders31_32/odev1.java
@@ -0,0 +1,28 @@
+package Ders31_32;
+
+import javax.sound.midi.SysexMessage;
+
+public class odev1 {
+    public static void main(String[] var0) {
+        //  1. ÖDEV 2'DEN 20'YE KADAR ÇİFT SAYILARı YAZDIR 
+        //  2. ÖDEV 10'DAN GERİYE 1'ER SAYARAK GİDİN
+
+        //  1. GÖREV 1'DEN 10'A KADAR TOPLAM
+        int toplam = 0;
+
+        for (int i = 1; i <=10;i++){
+            toplam += i;
+            System.out.println("Sayaç: " + i + " | Sayaç Sonrası Toplam " + toplam);    
+
+        }
+        //  2. GÖREV 1'DEN 10' KADAR ÇARPTIM TOPLAMINI
+
+        //  3. 1'DEN 100'E KADAR OLAN SAYILARI EKRANA YAZDIRACAK BİR PROGRAM
+        //      - SAYI 3'E TAM BÖLÜNÜYORSA SAYININ KENDİSİ YERİNE "Fizz" yazdırın.
+        //      - SAYI 5'E TAM BÖLÜNÜYORSA SAYININ KENDİSİ YERİNE "Buzz" yazdırın.
+        //      - SAYI HEM 3'E HEM DE 5'E TAM BÖLÜNÜYORSA "FizzBuzz" yazdırın.
+        // for döngüsü + if else if else ve %
+                
+    }
+    
+}
