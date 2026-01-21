6823efdd11d355383e4fa9ec00a9cc68157aa5d5
Burak Üstün <throborexz@gmail.com>
2026-01-21 20:07:23 +0300

    s
    s

---
+++ Ders55_56/Oyuncu.java
@@ -0,0 +1,19 @@
+package Ders55_56;
+
+public class Oyuncu {
+
+    // Gereksinimler:
+    // Özellikler (Instance): Her oyuncunun bir kullaniciAdi ( String ) ve bir skor ’u ( int ) olmalı.
+
+    // Paylaşılan Özellik (Static): Tüm oyuncular için geçerli olan oyunAdi ( String ) adında tek bir değişken olmalı.
+
+    // Constructor: Oyuncu, kullanıcı adıyla yaratılmalı ve skoru 0 olarak başlamalı. 
+    
+    // Davranışlar (Metotlar):
+    // getKullaniciAdi() , getSkor() gibi getter’lar.
+    // public static String getOyunAdi() gibi bir static getter.
+    // public void skorArttir(int puan) : Oyuncunun skorunu verilen puan kadar artıran bir metot.
+    // public boolean rekorKirdi(int rekorSkor) : Oyuncunun skorunun, parametre olarak
+    // gelen rekor skordan yüksek olup olmadığını kontrol eden bir metot.
+    
+}
