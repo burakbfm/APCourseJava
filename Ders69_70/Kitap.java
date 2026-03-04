ed48766f04e4ce79e5e767571e4ee24c7da35b50
Burak Üstün <throborexz@gmail.com>
2026-03-04 21:33:21 +0300

    s
    s

---
+++ Ders69_70/Kitap.java
@@ -0,0 +1,25 @@
+package Ders69_70;
+
+public class Kitap {
+    private int sayfaSayisi;
+    private String kitapTuru;
+    private String isim;
+
+    public Kitap (int ss, String t, String i) {
+        this.sayfaSayisi = ss;
+        this.kitapTuru = t;
+        this.isim = i;
+    }
+
+    public int sayfasayisiver() {
+        return this.sayfaSayisi;
+    }
+
+    public String turuver() {
+        return this.kitapTuru;
+    }
+
+    public String isimver() {
+        return this.isim;
+    }
+}
---
+++ Ders69_70/kutuphaneAnaliz.java
@@ -0,0 +1,33 @@
+package Ders69_70;
+
+import java.util.ArrayList;
+
+public class kutuphaneAnaliz {
+    public static void main(String[] var0) {
+
+        ArrayList<Kitap> kutuphane = new ArrayList<>();
+        kutuphane.add(new Kitap(300, "roman","Derbeder adam"));
+        kutuphane.add(new Kitap(420, "şiir","Kaybettiğim Yıllar"));
+        kutuphane.add(new Kitap(220, "roman", "Azabın pençesi"));
+        kutuphane.add(new Kitap(200, "bilim","Kozmos"));
+
+        // Roman türündeki Kitapların toplam sayfa sayısı:
+
+        int toplamRomanSayfasi = 0;
+
+        for (Kitap kitap : kutuphane) {
+            if (kitap.turuver().equals("roman")) {
+                toplamRomanSayfasi += kitap.sayfasayisiver();
+            }
+        }
+        
+        for (Kitap kitap : kutuphane) {
+            if (kitap.sayfasayisiver() >= 300) {
+                System.out.println("Sayfa Sayısı 300'den büyük olan kitap:" + kitap.isimver());
+            }
+        }
+
+        System.out.println("Kütüphanedeki roman türündeki kitapların toplam sayfa sayısı: " + toplamRomanSayfasi);
+
+    }
+}
